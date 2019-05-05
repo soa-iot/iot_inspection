3a541e743ce24f0dd45ab78d5f496b5919078084
chenyulin <chenyulin@chenyulin>
2019-05-05 17:13:19 +0800

    一类、二类动设备巡检记录筛选条件更新

--- src/main/java/cn/zg/entity/daoEntity/MovingEquipmentValue.java
+++ src/main/java/cn/zg/entity/daoEntity/MovingEquipmentValue.java
@@ -1,118 +1,187 @@
 package cn.zg.entity.daoEntity;
 
-
 public class MovingEquipmentValue {
-    private String id;
+	private String id;
 
-    private String schemeRemark;
+	private String schemeRemark;
 
-    private String requireId;
+	private String requireId;
 
-    private String paraValue;
+	private String paraValue;
 
-    private String unit;
+	private String unit;
 
-    private String positionNum;
+	private String positionNum;
 
-    private String equName;
+	private String equName;
 
-    private String standby1;
+	private String standby1;
 
-    private String standby2;
+	private String standby2;
 
-    private String standby3;
+	private String standby3;
 
-    private String recordDate;
+	private String recordDate;
 
-    public String getId() {
-        return id;
-    }
+	private String paraName;
 
-    public void setId(String id) {
-        this.id = id == null ? null : id.trim();
-    }
+	private String taskRequireContext;
 
-    public String getSchemeRemark() {
-        return schemeRemark;
-    }
+	private String standValue;
 
-    public void setSchemeRemark(String schemeRemark) {
-        this.schemeRemark = schemeRemark == null ? null : schemeRemark.trim();
-    }
+	private String taskInstId;
 
-    public String getRequireId() {
-        return requireId;
-    }
+	/**
+	 * @return the taskInstId
+	 */
+	public String getTaskInstId() {
+		return taskInstId;
+	}
 
-    public void setRequireId(String requireId) {
-        this.requireId = requireId == null ? null : requireId.trim();
-    }
+	/**
+	 * @param taskInstId
+	 *            the taskInstId to set
+	 */
+	public void setTaskInstId(String taskInstId) {
+		this.taskInstId = taskInstId;
+	}
 
-    public String getParaValue() {
-        return paraValue;
-    }
+	/**
+	 * @return the taskRequireContext
+	 */
+	public String getTaskRequireContext() {
+		return taskRequireContext;
+	}
 
-    public void setParaValue(String paraValue) {
-        this.paraValue = paraValue == null ? null : paraValue.trim();
-    }
+	/**
+	 * @return the standValue
+	 */
+	public String getStandValue() {
+		return standValue;
+	}
 
-    public String getUnit() {
-        return unit;
-    }
+	/**
+	 * @param taskRequireContext
+	 *            the taskRequireContext to set
+	 */
+	public void setTaskRequireContext(String taskRequireContext) {
+		this.taskRequireContext = taskRequireContext;
+	}
 
-    public void setUnit(String unit) {
-        this.unit = unit == null ? null : unit.trim();
-    }
+	/**
+	 * @param standValue
+	 *            the standValue to set
+	 */
+	public void setStandValue(String standValue) {
+		this.standValue = standValue;
+	}
 
-    public String getPositionNum() {
-        return positionNum;
-    }
+	/**
+	 * @return the paraName
+	 */
+	public String getParaName() {
+		return paraName;
+	}
 
-    public void setPositionNum(String positionNum) {
-        this.positionNum = positionNum == null ? null : positionNum.trim();
-    }
+	/**
+	 * @param paraName
+	 *            the paraName to set
+	 */
+	public void setParaName(String paraName) {
+		this.paraName = paraName;
+	}
 
-    public String getEquName() {
-        return equName;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public void setEquName(String equName) {
-        this.equName = equName == null ? null : equName.trim();
-    }
+	public void setId(String id) {
+		this.id = id == null ? null : id.trim();
+	}
 
-    public String getStandby1() {
-        return standby1;
-    }
+	public String getSchemeRemark() {
+		return schemeRemark;
+	}
 
-    public void setStandby1(String standby1) {
-        this.standby1 = standby1 == null ? null : standby1.trim();
-    }
+	public void setSchemeRemark(String schemeRemark) {
+		this.schemeRemark = schemeRemark == null ? null : schemeRemark.trim();
+	}
 
-    public String getStandby2() {
-        return standby2;
-    }
+	public String getRequireId() {
+		return requireId;
+	}
 
-    public void setStandby2(String standby2) {
-        this.standby2 = standby2 == null ? null : standby2.trim();
-    }
+	public void setRequireId(String requireId) {
+		this.requireId = requireId == null ? null : requireId.trim();
+	}
 
-    public String getStandby3() {
-        return standby3;
-    }
+	public String getParaValue() {
+		return paraValue;
+	}
 
-    public void setStandby3(String standby3) {
-        this.standby3 = standby3 == null ? null : standby3.trim();
-    }
+	public void setParaValue(String paraValue) {
+		this.paraValue = paraValue == null ? null : paraValue.trim();
+	}
 
-    public String getRecordDate() {
-        return recordDate;
-    }
+	public String getUnit() {
+		return unit;
+	}
 
-    public void setRecordDate(String recordDate) {
-        this.recordDate = recordDate;
-    }
+	public void setUnit(String unit) {
+		this.unit = unit == null ? null : unit.trim();
+	}
 
-	/* (non-Javadoc)
+	public String getPositionNum() {
+		return positionNum;
+	}
+
+	public void setPositionNum(String positionNum) {
+		this.positionNum = positionNum == null ? null : positionNum.trim();
+	}
+
+	public String getEquName() {
+		return equName;
+	}
+
+	public void setEquName(String equName) {
+		this.equName = equName == null ? null : equName.trim();
+	}
+
+	public String getStandby1() {
+		return standby1;
+	}
+
+	public void setStandby1(String standby1) {
+		this.standby1 = standby1 == null ? null : standby1.trim();
+	}
+
+	public String getStandby2() {
+		return standby2;
+	}
+
+	public void setStandby2(String standby2) {
+		this.standby2 = standby2 == null ? null : standby2.trim();
+	}
+
+	public String getStandby3() {
+		return standby3;
+	}
+
+	public void setStandby3(String standby3) {
+		this.standby3 = standby3 == null ? null : standby3.trim();
+	}
+
+	public String getRecordDate() {
+		return recordDate;
+	}
+
+	public void setRecordDate(String recordDate) {
+		this.recordDate = recordDate;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
@@ -120,8 +189,8 @@ public class MovingEquipmentValue {
 		return "MovingEquipmentValue [id=" + id + ", schemeRemark=" + schemeRemark + ", requireId=" + requireId
 				+ ", paraValue=" + paraValue + ", unit=" + unit + ", positionNum=" + positionNum + ", equName="
 				+ equName + ", standby1=" + standby1 + ", standby2=" + standby2 + ", standby3=" + standby3
-				+ ", recordDate=" + recordDate + "]";
+				+ ", recordDate=" + recordDate + ", paraName=" + paraName + ", taskRequireContext=" + taskRequireContext
+				+ ", standValue=" + standValue + ", taskInstId=" + taskInstId + "]";
 	}
-    
-    
+
 }
